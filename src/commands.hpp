e16058a34ed9f844f30a0dff20f4bd5bb0d3d2c4
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 18:17:48 +0300

    Update commands.hpp

--- src/commands.hpp
+++ src/commands.hpp
@@ -3,19 +3,14 @@
 
 #include "membrane.hpp"
 
-class Commands {
-public:
-    static std::vector<std::string> doit(std::vector<std::string> commands)
-    {
-        console_log("..all systems go..");
-        return commands;
-    }
+std::vector<std::string> doit(std::vector<std::string> commands)
+{
+    console_log("..all systems go..");
+    return commands;
+}
 
-    std::map<std::string, std::function<
-        std::vector<std::string>(std::vector<std::string>)
-    >> commandsMap = {
-        {"doit", doit}
-    };
+std::map<std::string, std::function<std::vector<std::string>(std::vector<std::string>)>> commandsMap = {
+    {"doit", doit}
 };
 
 #endif // _COMMANDS_HPP_
