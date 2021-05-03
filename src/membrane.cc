c79ad2835b4b0971800bb75e1fa6b5bf86542a54
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 15:32:04 +0300

    Update membrane.cc

--- src/membrane.cc
+++ src/membrane.cc
@@ -1,19 +1,5 @@
 #include "membrane.hpp"
-
-class Commands {
-public:
-    static void doit(std::vector<std::string> commands)
-    {
-        std::cout << "..all systems go.." << std::endl;
-        return commands;
-    }
-
-    std::map<std::string, std::function<
-        std::vector<std::string>(std::vector<std::string>)
-    >> commandsMap = {
-        {"doit", doit}
-    };
-};
+#include "commands.hpp"
 
 int membrane_connect(char *host, char *port)
 {
