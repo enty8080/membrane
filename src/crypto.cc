8c432aab1ae64c8f636a8136f385bce5c7db7aee
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 17:37:22 +0300

    Update crypto.cc

--- src/crypto.cc
+++ src/crypto.cc
@@ -1,15 +1,15 @@
 #include "crypto.hpp"
 
-/* VKS cypher here */
+/* VKS cipher here */
 
 std::string crypto(char *string)
 {
     std::string message(string);
     std::map<char, std::string> numsMap =
     {
-        {'0': "v"}, {'1': "k"}, {'2': "s"}, {'3': "h"},
-        {'4': "g"}, {'5': "f"}, {'6': "c"}, {'7': "z"}, {'8': "o"},
-        {'9': "x"}, {':', "d"}
+        {'0', "v"}, {'1', "k"}, {'2', "s"}, {'3', "h"},
+        {'4', "g"}, {'5', "f"}, {'6', "c"}, {'7', "z"}, {'8', "o"},
+        {'9', "x"}, {':', "d"}
     };
   
     for(char& c : message) {
@@ -26,9 +26,9 @@ std::string uncrypto(char *string)
     std::string message(string);
     std::map<char, std::string> numsMap =
     {
-        {'v': "0"}, {'k': "1"}, {'s': "2"}, {'h': "3"},
-        {'g': "4"}, {'f': "5"}, {'c': "6"}, {'z': "7"}, {'o': "8"},
-        {'x': "9"}, {'d', ":"}
+        {'v', "0"}, {'k', "1"}, {'s', "2"}, {'h', "3"},
+        {'g', "4"}, {'f', "5"}, {'c', "6"}, {'z', "7"}, {'o', "8"},
+        {'x', "9"}, {'d', ":"}
     };
   
     for(char& c : message) {
