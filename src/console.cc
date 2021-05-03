c23796534b6bb0a8855f71a5b1faadf831f79d2e
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 16:06:24 +0300

    Update console.cc

--- src/console.cc
+++ src/console.cc
@@ -1,6 +1,6 @@
 #include "console.hpp"
 
-void console_log(std::string message, bool newline = 1)
+void console_log(std::string message, bool newline)
 {
     if (newline)
         std::cout << message << std::endl;
@@ -8,27 +8,27 @@ void console_log(std::string message, bool newline = 1)
         std::cout << message;
 }
 
-void console_log_error(std::string message, bool newline = 1)
+void console_log_error(std::string message, bool newline)
 {
     console_log("[-] " + message, newline);
 }
 
-void console_log_process(std::string message, bool newline = 1)
+void console_log_process(std::string message, bool newline)
 {
     console_log("[*] " + message, newline);
 }
 
-void console_log_success(std::string message, bool newline = 1)
+void console_log_success(std::string message, bool newline)
 {
     console_log("[+] " + message, newline);
 }
 
-void console_log_warning(std::string message, bool newline = 1)
+void console_log_warning(std::string message, bool newline)
 {
     console_log("[!] " + message, newline);
 }
 
-void console_log_information(std::string message, bool newline = 1)
+void console_log_information(std::string message, bool newline)
 {
     console_log("[i] " + message, newline);
 }
