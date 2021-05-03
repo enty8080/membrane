cc090a95f4b15e5833cae7a73220358eae0596c2
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 22:26:38 +0300

    Update commands.cc

--- src/commands.cc
+++ src/commands.cc
@@ -2,16 +2,29 @@
 
 void rm(std::vector<std::string> commands)
 {
-    if (commands.size() == 2)
+    if (commands.size() > 1)
         if (std::remove(commands[1].c_str()) < 0)
             console_log_error("No such file!");
-    else if (commands.size() > 2 && (commands[1] == '-d' || commands[1] == '--directory'))
-        if (rmdir(commands[2].c_str()) < 0)
+    else
+        console_log("Usage: rm <file>");
+}
+
+void rmdir(std::vector<std::string> commands)
+{
+    if (commands.size() > 1)
+        if (rmdir(commands[1].c_str()) < 0)
             console_log_error("No such directory!");
-    else {
-        console_log("Usage: rm [options] <path>\n");
-        console_log("  -d, --directory  Remove specified directory instead of file.\n");
-    }
+    else
+        console_log("Usage: rmdir <directory>");
+}
+
+void mkdir(std::vector<std::string> commands)
+{
+    if (commands.size() > 1)
+        if (mkdir(commands[1].c_str()) < 0)
+            console_log_error("Directory already exists!");
+    else
+        console_log("Usage: mkdir <directory>");
 }
 
 void exec(std::vector<std::string> commands)
@@ -28,6 +41,16 @@ void cd(std::vector<std::string> commands)
         chdir(commands[1].c_str());
     else
         console_log("Usage: cd <directory>");
+}
+
+void touch(std::vector<std::string> commands)
+{
+    if (commands.size() > 1) {
+        std::fstream file;
+        file.open(commands[1].c_str(), ios::out);
+        file.close();
+    } else
+        console_log("Usage: touch <file>")
 }
 
 void shell()
