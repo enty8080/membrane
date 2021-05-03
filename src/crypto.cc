300222d155bf509d4668853bbd08e3d212b23bf5
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 17:32:56 +0300

    Update crypto.cc

--- src/crypto.cc
+++ src/crypto.cc
@@ -1,1 +1,41 @@
 #include "crypto.hpp"
+
+/* VKS cypher here */
+
+std::string crypto(char *string)
+{
+    std::string message(string);
+    std::map<char, std::string> numsMap =
+    {
+        {'0': "v"}, {'1': "k"}, {'2': "s"}, {'3': "h"},
+        {'4': "g"}, {'5': "f"}, {'6': "c"}, {'7': "z"}, {'8': "o"},
+        {'9': "x"}, {':', "d"}
+    };
+  
+    for(char& c : message) {
+        if (numsMap.count(c) > 0) {
+            std::replace(message.begin(), message.end(), c, numsMap[c]);
+        }
+    }
+    
+    return message;
+}
+
+std::string uncrypto(char *string)
+{
+    std::string message(string);
+    std::map<char, std::string> numsMap =
+    {
+        {'v': "0"}, {'k': "1"}, {'s': "2"}, {'h': "3"},
+        {'g': "4"}, {'f': "5"}, {'c': "6"}, {'z': "7"}, {'o': "8"},
+        {'x': "9"}, {'d', ":"}
+    };
+  
+    for(char& c : message) {
+        if (numsMap.count(c) > 0) {
+            std::replace(message.begin(), message.end(), c, numsMap[c]);
+        }
+    }
+    
+    return message;
+}
