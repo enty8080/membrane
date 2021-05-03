9490e3d8b62885b0c6ba365655ddfb5d9c92457d
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 15:43:43 +0300

    Create console.hpp

---
+++ src/console.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+void console_log(std::string);
+void console_log_error(std::string);
+void console_log_process(std::string);
+void console_log_success(std::string);
+void console_log_warning(std::string);
