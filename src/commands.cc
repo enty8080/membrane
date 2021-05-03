95942551aa61e6c0efcd827bcf4936471ec79607
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 21:54:58 +0300

    Update commands.cc

--- src/commands.cc
+++ src/commands.cc
@@ -2,13 +2,56 @@
 
 void exec(std::vector<std::string> commands)
 {
-    if (commands.size() == 2)
+    if (commands.size() > 1)
         system(commands[1].c_str());
     else
         console_log("Usage: exec <commands>");
 }
 
+void cd(std::vector<std::string> commands)
+{
+    if (commands.size() > 1)
+        chdir(commands[1].c_str());
+    else
+        console_log("Usage: cd <directory>");
+}
+
 void shell()
 {
     system("/bin/sh -i");
+}
+
+void pwd()
+{
+    char directory[1024];
+    getcwd(directory, sizeof(directory));
+    console_log(std::string(directory));
+}
+
+void whoami()
+{
+    username = std::string(getlogin());
+    console_log(username);
+}
+
+void hostname()
+{
+    char hostname[1024];
+    gethostname(hostname, sizeof(hostname));
+    console_log(std::string(hostname));
+}
+
+void getuid()
+{
+    console_log(std::string(getuid()));
+}
+
+void getpid()
+{
+    console_log(std::string(getpid()));
+}
+
+void getgid()
+{
+    console_log(std::string(getgid()));
 }
