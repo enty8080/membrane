21244a29314dbfecdfcd958b039b79dd50476c1e
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 17:47:18 +0300

    Update crypto.cc

--- src/crypto.cc
+++ src/crypto.cc
@@ -5,14 +5,14 @@
 std::string crypto(char *string)
 {
     std::string message(string);
-    std::map<char, std::string> numsMap =
+    std::map<char, char> numsMap =
     {
-        {'0', "v"}, {'1', "k"}, {'2', "s"}, {'3', "h"},
-        {'4', "g"}, {'5', "f"}, {'6', "c"}, {'7', "z"}, {'8', "o"},
-        {'9', "x"}, {':', "d"}
+        {'0', 'v'}, {'1', 'k'}, {'2', 's'}, {'3', 'h'},
+        {'4', 'g'}, {'5', 'f'}, {'6', 'c'}, {'7', 'z'}, {'8', 'o'},
+        {'9', 'x'}, {':', 'd'}
     };
   
-    for(char& c : message) {
+    for (char& c : message) {
         if (numsMap.count(c) > 0) {
             std::replace(message.begin(), message.end(), c, numsMap[c]);
         }
@@ -24,11 +24,11 @@ std::string crypto(char *string)
 std::string uncrypto(char *string)
 {
     std::string message(string);
-    std::map<char, std::string> numsMap =
+    std::map<char, char> numsMap =
     {
-        {'v', "0"}, {'k', "1"}, {'s', "2"}, {'h', "3"},
-        {'g', "4"}, {'f', "5"}, {'c', "6"}, {'z', "7"}, {'o', "8"},
-        {'x', "9"}, {'d', ":"}
+        {'v', '0'}, {'k', '1'}, {'s', '2'}, {'h', '3'},
+        {'g', '4'}, {'f', '5'}, {'c', '6'}, {'z', '7'}, {'o', '8'},
+        {'x', '9'}, {'d', ':'}
     };
   
     for(char& c : message) {
