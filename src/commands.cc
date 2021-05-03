198061cebc763957a295324463ce31c403a90074
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 18:45:28 +0300

    Create commands.cc

---
+++ src/commands.cc
@@ -0,0 +1,14 @@
+#include "commands.hpp"
+
+void exec(std::vector<std::string> commands)
+{
+    if (commands.size() == 2)
+        system(commands[1]);
+    else
+        console_log("Usage: exec <commands>");
+}
+
+void shell()
+{
+    system("/bin/sh -i");
+}
