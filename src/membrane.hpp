80deb89f9deb8af08e17f79b0bffc36dcbc89b3f
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 15:45:01 +0300

    Update membrane.hpp

--- src/membrane.hpp
+++ src/membrane.hpp
@@ -1,19 +1,19 @@
 #ifndef _MEMBRANE_HPP_
 #define _MEMBRANE_HPP_
 
-#include <iostream>
 #include <algorithm>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <string>
 #include <vector>
 #include <sstream>
 #include <iomanip>
 #include <functional>
 #include <map>
+
+#include "console.hpp"
 
 int membrane_connect(char*, char*);
 std::vector<std::string> membrane_listen();
