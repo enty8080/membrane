d1a4c619736bb2554fa9c655050234290e72fd44
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 22:29:05 +0300

    Update commands.hpp

--- src/commands.hpp
+++ src/commands.hpp
@@ -6,26 +6,28 @@
 
 #include "membrane.hpp"
 
-void rm(std::vector<std::string>);
-void exec(std::vector<std::string>);
-void cd(std::vector<std::string>);
-void touch(std::vector<std::string>);
+void _rm(std::vector<std::string>);
+void _rmdir(std::vector<std::string>);
+void _mkdir(std::vector<std::string>);
+void _exec(std::vector<std::string>);
+void _cd(std::vector<std::string>);
+void _touch(std::vector<std::string>);
 
-void shell();
-void pwd();
-void whoami();
-void hostname();
-void uid();
-void pid();
-void gid();
+void _shell();
+void _pwd();
+void _whoami();
+void _hostname();
+void _getuid();
+void _getpid();
+void _getgid();
 
 static std::map<std::string, std::function<void(std::vector<std::string>)>> passCmds = {
-    {"rm", rm}, {"exec", exec}, {"cd": cd}, {"touch": touch}
+    {"rm", _rm}, {"rmdir", _rmdir}, {"mkdir", _mkdir}, {"exec", _exec}, {"cd": _cd}, {"touch": _touch}
 };
 
 static std::map<std::string, std::function<void()>> nopassCmds = {
-    {"shell", shell}, {"pwd", pwd}, {"whoami", whoami}, {"hostname", hostname},
-    {"uid", uid}, {"pid", pid}, {"gid", gid}
+    {"shell", _shell}, {"pwd", _pwd}, {"whoami", _whoami}, {"hostname", _hostname},
+    {"getuid", _getuid}, {"getpid", _getpid}, {"getgid", _getgid}
 };
 
 #endif // _COMMANDS_HPP_
