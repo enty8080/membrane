1af4d52093c09d471e959d6725c3a8dafb26e437
Ivan Nikolsky <enty8080@gmail.com>
2021-05-03 15:57:47 +0300

    Update console.hpp

--- src/console.hpp
+++ src/console.hpp
@@ -4,11 +4,11 @@
 #include <iostream>
 #include <string>
 
-void console_log(std::string, bool = 1);
-void console_log_error(std::string, bool = 1);
-void console_log_process(std::string, bool = 1);
-void console_log_success(std::string, bool = 1);
-void console_log_warning(std::string, bool = 1);
-void console_log_information(std::string, bool = 1);
+void console_log(std::string, bool newline = 1);
+void console_log_error(std::string, bool newline = 1);
+void console_log_process(std::string, bool newline = 1);
+void console_log_success(std::string, bool newline = 1);
+void console_log_warning(std::string, bool newline = 1);
+void console_log_information(std::string, bool newline = 1);
 
 #endif // _CONSOLE_HPP_
