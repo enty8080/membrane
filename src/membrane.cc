3b0a3b2b7229a2deae1b5f151f72309642ffdf6b
Ivan Nikolsky <enty8080@gmail.com>
2021-05-05 00:11:03 +0300

    Update membrane.cc

--- src/membrane.cc
+++ src/membrane.cc
@@ -3,18 +3,16 @@
 int membrane_connect(std::string host, int port)
 {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
+    if (sock == -1)
         return -1;
-    }
 
     sockaddr_in hint;
     hint.sin_family = AF_INET;
     hint.sin_port = htons(port);
     inet_pton(AF_INET, host.c_str(), &hint.sin_addr);
 
-    if (connect(sock, (sockaddr*)&hint, sizeof(hint)) == -1) {
+    if (connect(sock, (sockaddr*)&hint, sizeof(hint)) == -1)
         return -2;
-    }
 
     dup2(sock, 0);
     dup2(sock, 1);
@@ -23,7 +21,34 @@ int membrane_connect(std::string host, int port)
     return sock;
 }
 
-std::vector<std::string> membrane_listen()
+int membrane_listen(int port)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+        return -1;
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_addr.s_addr = INADDR_ANY;
+    hint.sin_port = htons(port);
+
+    if (bind(sock, (struct sockaddr*)&hint, sizeof(hint)) < 0)
+        return -2;
+
+    if (listen(sock, 10) < 0)
+        return -3;
+
+    auto addrlen = sizeof(sockaddr);
+    int newsock = accept(sock, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
+
+    dup2(newsock, 0);
+    dup2(newsock, 1);
+    dup2(newsock, 3);
+
+    return newsock;
+}
+
+std::vector<std::string> membrane_read()
 {
     std::string command;
     std::getline(std::cin, command);
