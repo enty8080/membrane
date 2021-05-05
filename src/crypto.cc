8763306731a0ef1bbc3782b9b70c638a7a08ab23
Ivan Nikolsky <enty8080@gmail.com>
2021-05-05 03:28:43 +0300

    Update crypto.cc

--- src/crypto.cc
+++ src/crypto.cc
@@ -1,18 +1,9 @@
 #include "crypto.hpp"
 
-#ifndef KEY
-#define KEY "alena"
-#endif
-
 std::string crypto(std::string string)
 {
-    size_t string_length = string.length();
-    size_t key_length = std::string(KEY).length();
-
-    for (char& c : string) {
-        char magic = char(int(c) ^ key_length);
-        std::replace(string.begin(), string.end(), c, magic);
-    }
-
+    std::string result = "";
+    for (char& c : string)
+        result += char(int(c) ^ string.length());
     return string;
 }
